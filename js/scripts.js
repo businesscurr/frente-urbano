93cc5c71b9c41ef72f3f99bf00bfd10b37b9cd10
businesscurr <72956558+businesscurr@users.noreply.github.com>
2021-10-19 18:33:58 -0400

    Actualización

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,23 @@ let btnLogin                = document.querySelector('#btnLogin'),
     btnAccordeonRegistro    = document.querySelector('.btnAccordeonRegistro'),
     arrowUpRegistro         = document.querySelector('.arrow-up-registro'),
     arrowUpRegistroActivo   = document.querySelector('.arrow-up-registro-activo'),
-    contenidoRegistro       = document.querySelector('.contenido-registro');
+    contenidoRegistro       = document.querySelector('.contenido-registro'),
+    ContainerImgProfile     = document.querySelector('.container-photo-profile'),
+    hideRecord_1            = document.querySelector('#hide-record-1'),
+    hideRecord_2            = document.querySelector('#hide-record-2'),
+    btnRegister1            = document.querySelector('#btn-register-1'),
+    btnRegister2            = document.querySelector('#btn-register-2'),
+    arrowRegistro           = document.querySelector('#arrow-registro'),
+    linkOlvidoPass          = document.querySelector('#link-olvido-pass'),
+    modalOlvidoContrasena   = document.querySelector('.modal-olvido-contrasena'),
+    closeModalOlvidoContrasenia = document.querySelector('#close-modal-olvido-contrasenia'),
+    btnHide1                = document.querySelector('#btnHide1'),
+    modalOlvidoPass2        = document.querySelector('.modal-olvido-pass2'),
+    closeModalHidePass2     = document.querySelector('.closeModalHidePass2'),
+    closeModalTermsConditions = document.querySelector('.closeModalTermsConditions'),
+    modalTermsAndConditions   = document.querySelector('.modal-terms-and-conditions'),
+    linkTermsConditions       = document.querySelector('#link-terms-conditions');
+    
  
 btnLogin.addEventListener('click', () => {
     modalLogin.classList.remove('d-none');
@@ -32,6 +48,7 @@ h2.forEach( ( cadaBtn , i )=>{
 
         bloque[i].classList.add('activo')
         btnCloseAccordeon.classList.add('d-flex');
+        ContainerImgProfile.classList.add('d-none');
 
     })
 });
@@ -40,6 +57,7 @@ btnCloseAccordeon.addEventListener('click', () => {
     bloque.forEach( ( cadaBloque , i )=>{
         bloque[i].classList.remove('activo');
         btnCloseAccordeon.classList.remove('d-flex');
+        ContainerImgProfile.classList.remove('d-none');
     })
 });
 
@@ -63,11 +81,51 @@ btnAccordeonRegistro.addEventListener('click', () => {
     arrowUpRegistro.classList.add('arrow-up-registro-activo')
     contenidoRegistro.classList.add('contenido-registro-activo')
     btnAccordeonRegistro.classList.add('btnAccordeonRegistro-active')
+
 });
 
 arrowUpRegistro.addEventListener('click', () => {
     arrowUpRegistro.classList.remove('arrow-up-registro-activo')
     contenidoRegistro.classList.remove('contenido-registro-activo')
     btnAccordeonRegistro.classList.remove('btnAccordeonRegistro-active')
+});
+
+btnRegister1.addEventListener('click', () => {
+    hideRecord_1.classList.add('d-none');
+    hideRecord_2.classList.add('d-block');
+    arrowUpRegistro.classList.remove('arrow-up-registro-activo');
+    arrowRegistro.classList.add('d-flex')
+});
+
+arrowRegistro.addEventListener('click', () => {
+    hideRecord_1.classList.remove('d-none');
+    hideRecord_2.classList.remove('d-block');
+    arrowUpRegistro.classList.add('arrow-up-registro-activo');
+    arrowRegistro.classList.remove('d-flex')
+});
+
+linkOlvidoPass.addEventListener('click', () => {
+    modalOlvidoContrasena.classList.add('d-block')
+});
+
+closeModalOlvidoContrasenia.addEventListener('click', () => {
+    modalOlvidoContrasena.classList.remove('d-block')
+});
+
+btnHide1.addEventListener('click', () => {
+    modalOlvidoPass2.classList.add('d-block')
+});
+
+closeModalHidePass2.addEventListener('click', () => {
+    modalOlvidoPass2.classList.remove('d-block')
+});
+
+linkTermsConditions.addEventListener('click', () => {
+    modalTermsAndConditions.classList.add('d-block');
+
+});
+
+closeModalTermsConditions.addEventListener('click', () => {
+    modalTermsAndConditions.classList.remove('d-block')
 });
 
